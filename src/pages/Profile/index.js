96854c831fe2841c2b8093ce31390a41d1e9b2bb
Douglas <douglasmartinssantoss@gmail.com>
2019-09-19 07:50:36 -0300

    pagina de perfil

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,7 +1,38 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { Form, Input } from "@rocketseat/unform";
 
-// import { Container } from './styles';
+import { Container } from "./styles";
 
 export default function Profile() {
-  return <div />;
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit(data) {}
+
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome completo" />
+        <Input name="email" type="email" placeholder="Seu endereço de e-mail" />
+
+        <hr />
+
+        <input
+          type="password"
+          name="oldPassword"
+          placeholder="Sua senha atual"
+        />
+        <input type="password" name="password" placeholder="Nova senha" />
+        <input
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirmação de senha"
+        />
+
+        <button type="button">Atualizar perfil</button>
+      </Form>
+
+      <button type="button">Sar do GoBarber</button>
+    </Container>
+  );
 }
